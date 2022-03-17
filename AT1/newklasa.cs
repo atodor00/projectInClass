7812c6813e4f23b36681edee922a2243a5dd35bd
atodor00 <atodor00@fesb.hr>
2022-03-17 19:50:18 +0100

    dodana metoda

---
+++ AT1/newklasa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AT1
+{
+    class newklasa
+    {
+        public void metoda() {
+            Console.WriteLine("Hello Worldd");
+        }
+    }
+}
