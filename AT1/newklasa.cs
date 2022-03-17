09f0b2dfa05b239d410f9d073cbe5150c9acc07e
atodor00 <atodor00@fesb.hr>
2022-03-17 20:01:20 +0100

    newklass

--- AT1/newklasa.cs
+++ AT1/newklasa.cs
@@ -6,13 +6,13 @@ namespace AT1
 {
     class newklasa
     {
-<<<<<<< HEAD
+
         public void metoda()
         {
             Console.WriteLine("Hello Worldd");
         }
-=======
+
         public int ajme=2;
->>>>>>> 9cdde8b8ff64ba25b06621337fee898f4c862cd6
+
     }
 }
