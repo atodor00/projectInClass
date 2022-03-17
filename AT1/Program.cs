df6d15abadeb979688b30a01e758c5d83c7e4ee0
dujeje <dujeradmanlivaja@gmail.com>
2022-03-17 19:21:07 +0100

    promjena

--- AT1/Program.cs
+++ AT1/Program.cs
@@ -6,7 +6,7 @@ namespace AT1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
